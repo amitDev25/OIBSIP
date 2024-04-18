59b850209be76dbac655ac832da88a7dd0eebf74
Amit Roy <110351067+amitDev25@users.noreply.github.com>
2024-04-18 21:29:33 +0530

    Add files via upload

---
+++ Task3_Temperature_Converter/script.js
@@ -0,0 +1,34 @@
+const convertBtn=()=> {
+            
+    let deg = document.getElementById("degree").value;
+    let unit = document.getElementById("unit").value;
+    let result = document.getElementById("result");
+    let deg2;
+    
+    
+
+
+    if (unit == 'celcius'){
+        deg2 = (deg * 1.8) + 32;
+        // console.log(deg2);
+        result.innerHTML = `${deg2} °F`;
+        result.style.color = "black";
+
+    }
+
+    else if (unit == 'fahrenheit'){
+        deg2 = (deg - 32) / 1.8;
+        // console.log(deg2);
+        result.innerHTML = `${deg2} °C`;
+        result.style.color = "black";
+
+
+    }
+
+    
+
+}
+
+document.getElementById("convert").addEventListener("click", (event)=>{
+    event.preventDefault();
+});
